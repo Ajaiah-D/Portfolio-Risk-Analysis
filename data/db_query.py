1a940c6b1a5209fec55c3f2e9ba084482dd66a0e
Ajaiah D. <ajaiahdarlington07@gmail.com>
2025-08-01 22:14:01 -0400

    query to test that data can be queried

--- data/db_query.py
+++ data/db_query.py
@@ -4,31 +4,22 @@ import pandas as pd
 # path to your database
 DB_PATH = "./data/portfolio_data.db"
 
-# list of test tickers
-test_tickers = ['MMM', 'AOS', 'ABT', 'ABBV', 'ACN']
+ticker = "AAPL"
 
 # connect to the database
 conn = sqlite3.connect(DB_PATH)
 
-# preview some data from daily_prices
-print("\n--- Sample from daily_prices ---")
-df_prices = pd.read_sql("SELECT * FROM daily_prices WHERE ticker IN ({}) LIMIT 10".format(
-    ','.join(['?']*len(test_tickers))
-), conn, params=test_tickers)
-print(df_prices)
+# Join daily_prices and tickers_meta for AAPL
+query = """
+SELECT p.*, m.name, m.sector
+FROM daily_prices p
+JOIN tickers_meta m ON p.ticker = m.ticker
+WHERE p.ticker = ?
+ORDER BY p.date DESC
+LIMIT 10
+"""
 
-# preview some data from tickers_meta
-print("\n--- Sample from tickers_meta ---")
-df_meta = pd.read_sql("SELECT * FROM tickers_meta WHERE ticker IN ({})".format(
-    ','.join(['?']*len(test_tickers))
-), conn, params=test_tickers)
-print(df_meta)
-
-# delete test tickers from both tables
-with conn:
-    conn.executemany("DELETE FROM daily_prices WHERE ticker = ?", [(t,) for t in test_tickers])
-    conn.executemany("DELETE FROM tickers_meta WHERE ticker = ?", [(t,) for t in test_tickers])
-
-print("\n Deleted test tickers from database.")
+df = pd.read_sql(query, conn, params=[ticker])
+print(df)
 
 conn.close()
